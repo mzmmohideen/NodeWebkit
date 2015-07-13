4684b97960dcd6f8e16809f15d5641f29b1cf757
mzmmohideen <mzmmohideen@gmail.com>
2015-07-13 19:24:54 +0530

    upload complete

--- attendanceapp/models.py
+++ attendanceapp/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.sessions.models import Session
 
 class TeacherProfile(models.Model):
 	staff_name = models.CharField(max_length=100)
 
 class StudentEntries(models.Model):
 	user = models.ForeignKey(TeacherProfile)
+	student_id = models.CharField(max_length=100)
 	student_name = models.CharField(max_length=100)
+	subject = models.CharField(max_length=100)
 	
 # Create your models here.
--- attendanceapp/views.py
+++ attendanceapp/views.py
@@ -2,14 +2,22 @@ from attendanceapp.models import *
 from django.core.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt,ensure_csrf_cookie
 import json,os
+from django.http import HttpResponse
 
 @csrf_exempt
 def enter_attendance(request):
-	print request
+	a = eval(dict(request.POST).keys()[0])
+	print a
+	if TeacherProfile.objects.filter(staff_name = a['username']):
+		staff = TeacherProfile.objects.get(staff_name = a['username'])
+		store = StudentEntries.objects.filter(user = staff,student_id = a['student_id']).update(subject=a['subject'],student_name=a['student_name']) if StudentEntries.objects.filter(user = staff,student_id = a['student_id']) else StudentEntries.objects.create(user = staff,student_id = a['student_id'],subject=a['subject'],student_name=a['student_name'])
+	else : 
+		new_staff = TeacherProfile.objects.create(staff_name = a['username'])
+		new_feed = StudentEntries.objects.filter(user = new_staff,student_id = a['student_id']).update(subject=a['subject'],student_name=a['student_name']) if StudentEntries.objects.filter(user = new_staff,student_id = a['student_id']) else StudentEntries.objects.create(user = new_staff,student_id = a['student_id'],subject=a['subject'],student_name=a['student_name'])
 	# c = {}
  #    c = csrf(request)
     # return redirect("search.html", c)
 	print '???'
-	return 'nothing'
+	return HttpResponse(content=json.dumps({"data": "api feed"}), content_type='application/json')            
 
 # Create your views here.
